b4386e38ef42301aa9a50afa5848e8fb150fec1f
kayo <kucd.1230@gmail.com>
2026-06-08 17:56:15 +0900

    feat: TODOリストのステート実装

--- src/App.tsx
+++ src/App.tsx
@@ -1,122 +1,72 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
+import { useState, ChangeEvent } from 'react' // useStateは画面上で変化するデータを一時的に記憶しておく関数
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+// タスク1つの型を定義する
+interface Todo {
+  id: number;
+  text: string;
+}
+
+function app() {
+  // 入力欄の文字を記憶する(初期値は空っぽ)
+  const [inputText, setInputText] = useState<string>("")
+
+  // タスクの一覧を一時保持する(初期値は2つのタスクが入った配列)
+  const [todos, setTodos] = useState<Todo[]>(
+    [
+      {id: 1, text: "テスト1テスト1テスト1"},
+      {id: 2, text: "テスト2テスト2テスト2"}
+    ]
+  )
+
+  // 入力欄に文字が入力されたとき
+  const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setInputText(e.target.value)
+  }
+
+  // ｢追加｣ボタンがクリックされたとき
+  const handleAddTodo = () => {
+    if (inputText.trim() === "") return
+    // 新しいタスクを作る
+    const newTodo: Todo = {
+      id: Date.now(),
+      text: inputText
+    }
+
+    // タスク一覧のステート更新関数を使って新しいタスクをセットし、入力欄のステートを空にする
+    setTodos([...todos, newTodo])
+    setInputText("")
+  }
+
+  // 「削除」ボタンがクリックされたとき
+  const handleDeleteTodo = (id: number) => {
+    const filterTodos = todos.filter(todo => todo.id !== id)
+    setTodos(filterTodos)
+  }
 
   return (
-    <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+    <div>
+      <h1>My Todo List</h1>
 
-      <div className="ticks"></div>
+      {/* タスク入力エリア */}
+      <div>
+        <input type="text" placeholder="新しいタスクを入力" value={inputText} onChange={handleInputChange}/>
+        <button onClick={handleAddTodo}>追加</button>
+      </div>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
+      {/* タスク一覧エリア */}
+      <div>
+        <ul>
+          {todos.map(t => (
+            <li key={t.id}>
+              <span>{t.text}</span>
+              <button onClick={() => handleDeleteTodo(t.id)}>削除</button>
             </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
-    </>
+          ))}
+        </ul>
+      </div>
+    </div>
   )
 }
 
-export default App
+export default app
